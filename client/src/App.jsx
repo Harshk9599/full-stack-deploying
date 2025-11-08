bddc37bfe7c93e0bb4bb0cb618e8e36188ad6503
Harshk9599 <harshkashyap14321@gmail.com>
2025-11-08 16:26:37 +0530

    commit

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/message")
+    // fetch("/api/message")  --> for proxy server
+    .then((res) => res.json())
+    .then((data) => setMessage(data.message))
+    .catch((err) =>{
+      console.error("Error fetching message", err);
+    })
+  }, [])
+
+  return (
+    <>
+      <h1>Welcome to HK-Developers Frontend</h1>
+      <h2>data :- {message}</h2>
+    </>
+  )
+}
+
+export default App
