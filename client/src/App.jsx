4c18ba4303cb6eca3966e84c16790ae25312957d
Harsh Kumar <150929870+Harshk9599@users.noreply.github.com>
2025-11-08 19:00:40 +0530

    m

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,18 @@ function App() {
   const [message, setMessage] = useState("")
 
   useEffect(() => {
-    fetch("http://localhost:4000/api/message")
-    // fetch("/api/message")  --> for proxy server
-    .then((res) => res.json())
-    .then((data) => setMessage(data.message))
-    .catch((err) =>{
-      console.error("Error fetching message", err);
-    })
+    // Dynamically choose the backend URL
+    const API_BASE =
+      window.location.hostname === "localhost"
+        ? "http://localhost:4000"
+        : "http://deploy.smartdoorlocks.in"  // use https later when SSL is ready
+
+    fetch(`${API_BASE}/api/message`)
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => {
+        console.error("Error fetching message", err)
+      })
   }, [])
 
   return (
@@ -23,3 +28,31 @@ function App() {
 }
 
 export default App
+
+
+
+// import { useState, useEffect } from 'react'
+// import './App.css'
+
+// function App() {
+//   const [message, setMessage] = useState("")
+
+//   useEffect(() => {
+//     fetch("http://localhost:4000/api/message")
+//     // fetch("/api/message")  --> for proxy server
+//     .then((res) => res.json())
+//     .then((data) => setMessage(data.message))
+//     .catch((err) =>{
+//       console.error("Error fetching message", err);
+//     })
+//   }, [])
+
+//   return (
+//     <>
+//       <h1>Welcome to HK-Developers Frontend</h1>
+//       <h2>data :- {message}</h2>
+//     </>
+//   )
+// }
+
+// export default App
